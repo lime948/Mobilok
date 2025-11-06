d83a836d323591b6a923b21d09449b024cb9614b
OreskoZ <OreskoZ@301-03>
2025-11-06 11:02:02 +0100

    Add project files.

---
+++ Mobilok/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Osztalyalapok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Phon> mobilok = new List<Phon>();
+            #region Betöltés állományból
+            string[] sorok = File.ReadAllLines("mobiladatok.txt");
+            foreach (string sor in sorok)
+            {
+                mobilok.Add(new Phon(sor));
+            }
+            #endregion
+            Console.Write("Adja meg a mobil nevét: ");
+            string s = Console.ReadLine().ToLower();
+            bool van = false;
+            for (int i = 0; i < mobilok.Count; i++)
+            {
+                if (mobilok[i].megnev.ToLower() == s)
+                {
+                    Console.WriteLine(mobilok[i].ToString());
+                    van = true;
+                    break;
+                }
+            }
+            if (van == false)
+            {
+                Console.WriteLine("Nincs ilyen");
+            }
+        }
+    }
+
+    internal class Phon
+    {
+        public string megnev;
+        public string gyarto;
+        public decimal kijmeret;
+        public int tarhely;
+        public int ram;
+        public int akku;
+
+        public Phon() { }
+
+        public Phon(string megnev, string gyarto, decimal kijmeret, int tarhely, int ram, int akku)
+        {
+            this.megnev = megnev;
+            this.gyarto = gyarto;
+            this.kijmeret = kijmeret;
+            this.tarhely = tarhely;
+            this.ram = ram;
+            this.akku = akku;
+        }
+
+        public Phon(string sor)
+        {
+            string[] adatok = sor.Split(';');
+            megnev = adatok[0];
+            gyarto = adatok[1];
+            kijmeret = decimal.Parse(adatok[2]);
+            tarhely = int.Parse(adatok[3]);
+            ram = int.Parse(adatok[4]);
+            akku = int.Parse(adatok[5]);
+        }
+
+        public override string ToString()
+        {
+            return $"Megnevezés: {megnev}\n" +
+                $"Gyártó: {gyarto}\n" + 
+                $"Kijelző mérete (hüvelyk): {kijmeret}\n" +
+                $"Tárhely (Gb): {tarhely}\n" +
+                $"RAM (Gb): {ram}\n" +
+                $"Akkumlátor (mAh) {akku}";
+        }
+    }
+}
---
+++ Mobilok/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Mobilok")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Mobilok")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a2da27c5-84b2-4b07-9250-b6b2331268a8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
